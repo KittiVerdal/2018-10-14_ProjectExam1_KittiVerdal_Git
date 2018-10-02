a18025fb87cced0e855e9b3108f845f3a68c5be8
kitti@jbl.no <kitti@jbl.no>
2018-10-02 19:12:11 +0200

    Index-movie and countdown

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var countdownNumberEl = document.getElementById('countdown-number');
+var countdown = 8;
+
+countdownNumberEl.textContent = countdown;
+
+setInterval(function() {
+  countdown = --countdown <= 0 ? 8 : countdown;
+
+  countdownNumberEl.textContent = countdown;
+}, 1000);
